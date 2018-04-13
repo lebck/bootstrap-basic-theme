14e622428635caafd1b99c62e3bc291ae555b213
Leo Back <leo.back@yahoo.de>
2018-04-13 17:40:46 +0200

    Added CSS for Content-Area and Background-Image for Home Page

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php get_header(); ?>
 
-<body class="<?php echo $post->ID; ?>">
+<body class="<?php echo 'site-' . $post->ID; ?>">
 <?php get_template_part('template-parts/navigation/navigation', 'top'); ?>
 
     <div class="container">
         <div class="row">
-            <div class="col offset-md-7 col-md-5">
-                <h2><?php the_title() ?></h2>
-
+            <div class="col offset-lg-5 col-lg-7">
                 <?php while (have_posts()) : the_post(); ?>
 
                     <div class="entry-content-page">
